0635a148db98a028f972cf10ae174d649ff6c763
kdn0325 <sexy-ehdsud@nate.com>
2022-01-21 17:07:44 +0900

    blog-update

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,7 +3,7 @@ import "./Topbar.css"
 import {Link} from "react-router-dom"
 
 export default function TopBar() {
-    const user = false;
+    const user = true;
     return (
         <div className="top">
             <div className="topLeft">
