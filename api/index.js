895a53f6027ba9734e234de2100af5e45ce912da
kdn0325 <sexy-ehdsud@nate.com>
2022-02-03 17:11:08 +0900

    heroku-deploy

--- api/index.js
+++ api/index.js
@@ -39,14 +39,12 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen("5000",()=>{
-    console.log("Backend is running")
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 });
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static("client/build"));
-    
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-    });
-}
+app.listen(process.env.PORT || "5000",()=>{
+    console.log("Backend is running")
+});
--- client/README.md
+++
@@ -1,7 +0,0 @@
-# Blog-app for DeskTop
-
-- 리액트를 활용하여 컴포넌트를 학습하고 랜더링을 이해하였습니다.
-
-## --> [Link](https://kdn0325.github.io/blog-app/)
-
-### Initial Screen
